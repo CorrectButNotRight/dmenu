9b86b70649cc3473afad3e0a42e8a4b19d3880e6
Andrew Chin <correctbutnotright@outlook.com>
2019-11-16 23:11:13 -0500

    invert select colour

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#0f0f0f" },
-	[SchemeSel] = { "#eeeeee", "#cc00cc" },
+	[SchemeSel] = { "#0f0f0f", "#ff29fb" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
