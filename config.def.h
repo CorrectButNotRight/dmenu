ea8b4091d8c5e24ad822afa6de006a78d9f1150b
Andrew Chin <correctbutnotright@outlook.com>
2020-02-15 18:31:09 -0500

    colors

--- config.def.h
+++ config.def.h
@@ -6,13 +6,13 @@ static int topbar = 0;                      /* -b  option; if 0, dmenu appears a
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"DejaVu Sans Mono for Powerline:style=Bold:size=9"
+	"DejaVu Sans Mono:style=Bold:size=9"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt      = "dmenu";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#e5e5e5", "#0f0f0f" },
-	[SchemeSel] = { "#0f0f0f", "#3daee9" },
+	[SchemeNorm] = { "#a2a2a2", "#0f0f0f" },
+	[SchemeSel] = { "#fba922", "#333333" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
